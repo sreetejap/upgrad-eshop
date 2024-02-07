6e6fc75dcae0f0aaa9aaa2b37b4828b17312b0fb
Sree Teja <potlurusreeteja1997@gmail.com>
2024-02-07 18:04:24 +0530

    fix: component name change for Login compnent

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -33,7 +33,7 @@ function Copyright(props) {
 
 const defaultTheme = createTheme();
 
-export default function SignIn() {
+export default function Login() {
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
